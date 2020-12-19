00bb68f9aa594aa01bdc6fb9825bb52c78a33251
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-12-19 18:24:10 +0300

    create article comments scaffolds

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :articles
+  has_many :comments
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :articles
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
