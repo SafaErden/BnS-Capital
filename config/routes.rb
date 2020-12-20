e6d5a696bcb9fd98186c43808b64992c095ea7a2
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-12-20 21:37:29 +0300

    add commenting

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
   resources :articles
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-=begin
-  devise_scope :user do
-    root to: "devise/sessions#new"
+  resources :articles do
+  	resources :comments
   end
-=end
 root to: "articles#index"
 end
